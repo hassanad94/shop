f0da9d819cd5e6ad6c33c1ef891eb143a21483a1
unknown <hassanad94@gmail.com>
2022-07-18 20:56:40 +0200

    upload current status

---
+++ pages/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { client } from '../lib/client';// in react useEffect to fatch data
+import { Product, FooterBanner, HeroBanner } from '../components' ;
+import banner from '../sanity_ecommerce/schemas/banner';
+
+//serverSide Renderből jönnek a bemenők
+const Home = ( {products, bannerData} ) => {
+  return (
+    <>
+      <HeroBanner heroBanner={bannerData && bannerData[0] }/>
+
+      <div className='products-heading'>
+
+        <h2>Legkeresettebb Termékek</h2>
+        <p>Rentgeteg formában megtalálható.</p>
+
+        <div className='products-container'>
+          {/* ?= check if not null */}
+          {products?.map( (product) => <Product key={product.id} product={product}/> )}
+
+
+        </div>
+
+      </div>
+      <FooterBanner/>
+    </>
+  )
+}
+//next js specific, fatching data... Betölti az adatokot a Dombba, gets populated into to a fuunction. máshol fogom tudni hasznáni
+export const getServerSideProps = async() =>{
+
+  //all products from sanity dashbor (sanitiy specific)
+  const query = '*[_type == "product"]';
+  const products =  await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData =  await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData }
+  }
+
+}
+
+export default Home
